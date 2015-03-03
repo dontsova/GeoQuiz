44e050746b19bd1983356b0826ee94b8f6a9c236
dontsova <v.koroliev@gmail.com>
2015-03-03 18:00:45 +0200

    add functionality to CheatActivity

--- src/com/example/geoquiz/CheatActivity.java
+++ src/com/example/geoquiz/CheatActivity.java
@@ -2,15 +2,41 @@ package com.example.geoquiz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by Слава on 03.03.2015.
  */
 public class CheatActivity extends Activity {
 
+    public static final String EXTRA_ANSWER_IS_TRUE = "com.example.geoquiz.answer_is_true";
+
+    private Button mShowAnswerButton;
+    private boolean mAnswerIsTrue;
+    private TextView mAnswerTextView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cheat_layout);
+
+        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
+
+        mAnswerTextView = (TextView)findViewById(R.id.answerTextView);
+
+        mShowAnswerButton = (Button)findViewById(R.id.showAnswerButton);
+        mShowAnswerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mAnswerIsTrue) {
+                    mAnswerTextView.setText(R.string.true_button);
+                } else {
+                    mAnswerTextView.setText(R.string.false_button);
+                }
+            }
+        });
     }
 }
